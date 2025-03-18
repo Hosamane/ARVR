cc23fd8ae82e511cf7d6e2b4edd69d58059849d6
Hosamane Veerabhadrappa Setty <140387249+Hosamane@users.noreply.github.com>
2025-03-18 11:42:03 +0530

    Update prog2.cs

--- prog2.cs
+++ prog2.cs
@@ -1,36 +1,51 @@
-using UnityEngine; 
-using UnityEngine.UI; 
- 
-public class ChangeAppearance : MonoBehaviour 
-{ 
-    public GameObject cube, sphere, plane;  // Assign objects in the Inspector 
-    public Material newCubeMaterial, newSphereMaterial, newPlaneMaterial;  // New materials 
-    public Texture newCubeTexture, newSphereTexture, newPlaneTexture;  // New textures 
- 
-    // Function to change Cube appearance 
-    public void ChangeCubeAppearance() 
-    { 
-        Renderer cubeRenderer = cube.GetComponent<Renderer>(); 
-        cubeRenderer.material = newCubeMaterial;  // Change Material 
-        cubeRenderer.material.mainTexture = newCubeTexture;  // Change Texture 
-        cubeRenderer.material.color = Color.red;  // Change Color 
-    } 
- 
-    // Function to change Sphere appearance 
-    public void ChangeSphereAppearance() 
-    { 
-        Renderer sphereRenderer = sphere.GetComponent<Renderer>(); 
-        sphereRenderer.material = newSphereMaterial; 
-        sphereRenderer.material.mainTexture = newSphereTexture; 
-        sphereRenderer.material.color = Color.green; 
-    } 
- 
-    // Function to change Plane appearance 
-    public void ChangePlaneAppearance() 
-    { 
-        Renderer planeRenderer = plane.GetComponent<Renderer>(); 
-        planeRenderer.material = newPlaneMaterial; 
-        planeRenderer.material.mainTexture = newPlaneTexture; 
-        planeRenderer.material.color = Color.blue; 
-    } 
-} 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Change : MonoBehaviour
+{
+    public GameObject plane, cube, sphere;
+    public Material newPlaneMaterial, newCubeMaterial, newSphereMaterial;
+    public Texture newPlaneTexture, newCubeTexture, newSphereTexture;
+
+    private Renderer cubeR;
+    private Renderer sphereR;
+    private Renderer planeR;
+
+    void Start() {
+        cubeR = cube.GetComponent<Renderer>();
+        sphereR = sphere.GetComponent<Renderer>();
+        planeR = plane.GetComponent<Renderer>();
+    }
+
+    public void ChangeColor() {
+        cubeR.material = new Material(Shader.Find("Standard"));
+        sphereR.material = new Material(Shader.Find("Standard"));
+        planeR.material = new Material(Shader.Find("Standard"));
+        cubeR.material.mainTexture = null;
+        sphereR.material.mainTexture = null;
+        planeR.material.mainTexture = null;
+        cubeR.material.color = Color.red;
+        sphereR.material.color = Color.green;
+        planeR.material.color = Color.blue;
+    }
+
+    public void ChangeMaterial() {
+        cubeR.material.mainTexture = null;
+        sphereR.material.mainTexture = null;
+        planeR.material.mainTexture = null;
+        cubeR.material = newCubeMaterial;
+        sphereR.material = newSphereMaterial;
+        planeR.material = newPlaneMaterial;
+    }
+
+    public void ChangeTexture() {
+        cubeR.material = new Material(Shader.Find("Standard"));
+        sphereR.material = new Material(Shader.Find("Standard"));
+        planeR.material = new Material(Shader.Find("Standard"));
+        cubeR.material.mainTexture = newCubeTexture;
+        sphereR.material.mainTexture = newSphereTexture;
+        planeR.material.mainTexture = newPlaneTexture;
+    }
+}
